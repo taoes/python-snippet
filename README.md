f3545c7ed61f59f639fbb6d3aed7d7cc3d739426
周涛 <zhoutao@uniontech3d.cn>
2019-09-19 10:14:29 +0800

    feat: 新增readme 文件  && helloWorld 示例程序

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Python中文学习笔记
 
 
+## 基础语法
++ 自带的诗歌 [src/base/hello_world.py](src/base/poem.py)
++ Hello world [src/base/hello_world.py](src/base/hello_world.py)
+
+## 文件IO
++ 创建文件并写入 [src/file/read_file.py](src/file/read_file.py)
--- src/file/read_file.py
+++ src/file/read_file.py
@@ -1,8 +1,17 @@
+import os
+
+# the path is linux style
+# if you use windows os ,the path need replace windows style like `C://Document/text.txt`
+file_path = '/tmp/text.txt'
 
 
-def main():
-    print("Hello,World")
+def write_file():
+    with open(file_path, 'w') as data_file:
+        data_file.write("人生若只如初见，何事悲风秋画扇\n")
+        data_file.write("等闲识得东风面，万紫千红总是春\n")
+    print("文件写入完成，下面使用的是shell 执行的脚本(仅支持linux and unix*)：\n")
+    os.system("cat " + file_path)
 
 
 if __name__ == '__main__':
-    main()
+    write_file()
